1eebe0a2754a7d04459491305d72ee66b30b9925
Mass <opfan@hotmail.fr>
2020-02-28 12:52:48 +0100

    fix robot_socket

--- scripts/robot_socket.py
+++ scripts/robot_socket.py
@@ -3,7 +3,7 @@ import asyncio
 import json
 import sys
 
-#import robotdeplacement as robot
+import robot_deplacement as robot
 import websockets
 
 
@@ -17,14 +17,15 @@ async def main_loop(ws):
     while True:
         data = await ws.recv()
         print(data)
-        #actions = get_actions_from_data(data)
-        #robot.perform_actions(actions)
+        actions = get_actions_from_data(data)
+        robot.perform_actions(actions)
         # todo send mission performed response
 
 
 
-async def connect_robot(ip, port):
-    uri = "ws://" + ip + ":" + port + "/robotsocket/"
+async def connect_robot(ip):
+    uri = "ws://" + ip + "/robotsocket/"
+    print("uri : ",uri)
     with open("conf.json", "r") as jsonfile:
         data = jsonfile.read()
     
@@ -41,10 +42,10 @@ async def connect_robot(ip, port):
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("usage: robot_socket address port")
+    if len(sys.argv) != 2:
+        print("usage: robot_socket websocketip")
         return
-    asyncio.get_event_loop().run_until_complete(connect_robot(sys.argv[1], sys.argv[2]))
+    asyncio.get_event_loop().run_until_complete(connect_robot(sys.argv[1]))
 
 
 
