ff1248621581fff078b5b0fa33029daed09729ad
Pavell <pavell94000@gmail.com>
2020-03-04 11:34:09 +0100

    reverted websocket code

--- apps/robot/websocket.py
+++ apps/robot/websocket.py
@@ -1,7 +1,8 @@
 import json
 
 import logging
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
 from django.conf import settings
 
 from robot.models import RobotModel
@@ -11,31 +12,31 @@ logger = logging.getLogger(__name__)
 
 
 
-class RobotConsumer(AsyncJsonWebsocketConsumer):
+class RobotConsumer(WebsocketConsumer):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.model = None
     
     
-    async def connect(self):
-        await self.accept()
+    def connect(self):
+        self.accept()
     
     
-    async def receive(self, text_data=None, bytes_data=None, **kwargs):
+    def receive(self, text_data=None, bytes_data=None):
         if self.model is None:
             try:
                 data = json.loads(text_data)
             except json.JSONDecodeError:
-                await self.close(code=3001)  # Invalid JSON
+                self.close(code=3001)  # Invalid JSON
                 return
             
             if "uuid" not in data or "type" not in data:
-                await self.close(code=3002)  # JSON must have fields : uuid, type
+                self.close(code=3002)  # JSON must have fields : uuid, type
                 return
             
             if data["type"] not in settings.ROBOT_CONFIGS:
-                await self.close(code=3003)  # Robot type not handled
+                self.close(code=3003)  # Robot type not handled
                 return
             
             try:
@@ -43,19 +44,19 @@ class RobotConsumer(AsyncJsonWebsocketConsumer):
             except RobotModel.DoesNotExist:
                 self.model = RobotModel.objects.create(uuid=data["uuid"], type=data["type"])
             self.model.connect(self.channel_name)
-            await self.channel_layer.group_add(str(self.model.uuid), self.channel_name)
-            await self.send(text_data="ok")
+            async_to_sync(self.channel_layer.group_add)(str(self.model.uuid), self.channel_name)
+            self.send(text_data="ok")
         else:
             logger.warning(f"Robot {self.model.uuid} sent : {text_data}")
             # TODO com with robot
     
     
-    async def disconnect(self, code):
+    def disconnect(self, code):
         if self.model:
-            await self.channel_layer.group_discard(str(self.model.uuid), self.channel_name)
+            async_to_sync(self.channel_layer.group_discard)(str(self.model.uuid), self.channel_name)
             self.model.disconnect()
     
     
-    async def mission(self, event):
+    def mission(self, event):
         message = event["text_data"]
-        await self.send(text_data=message)
+        self.send(text_data=message)
