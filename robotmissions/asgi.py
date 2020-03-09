0ea812b160dfeeabb3471007eb0222f0bbb3c1ff
Mass <opfan@hotmail.fr>
2020-03-09 16:46:18 +0100

    add app_dir at osgi run

--- robotmissions/asgi.py
+++ robotmissions/asgi.py
@@ -6,7 +6,15 @@ defined in the ASGI_APPLICATION setting.
 import os
 import django
 from channels.routing import get_default_application
+import sys
+
+from robotmissions import settings
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "robotmissions.settings")
+
+if settings.APPS_DIR not in sys.path:
+    sys.path.append(settings.APPS_DIR)
+    
 django.setup()
 application = get_default_application()
