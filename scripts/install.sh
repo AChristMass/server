9fb6878f45c8c1a6425c999e95ae1488242d100f
Christophe Callé <Pavell94000@users.noreply.github.com>
2020-03-22 17:03:42 +0100

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo ""
+echo "Installing requirements..."
+pip install -r requirements.txt || { echo>&2 "ERROR: pip3 install -r requirements.txt failed"; exit 1; }
+echo "Done !"
+
+echo ""
+echo "Creating RobotMissionIfc directory...."
+if [[ ! -d RobotMissionIfc ]]; then
+    mkdir RobotMissionIfc || { echo>&2 "ERROR: Can't create RobotMissionIfc" ; exit 1; }
+fi
+
+echo ""
+echo "Configuring database..."
+python3 manage.py makemigrations || { echo>&2 "ERROR: python3 manage.py makemigrations failed"; exit 1; }
+python3 manage.py migrate || { echo>&2 "ERROR: python3 manage.py migrate failed"; exit 1; }
+echo "Done !"
+
